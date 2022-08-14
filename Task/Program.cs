e180aac8f8123528ef0aab904911d0806fd5c874
Alex <skorpion_94@list.ru>
2022-08-14 13:59:23 +0300

    Дополнил файл Readme and написал программуу

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,3 +2,30 @@
 // длинна которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры. либо 
 // задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+void newArray(string[] Array, string[] NArray)
+{
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            NArray[j] = Array[i];
+            j++;
+        }
+    }
+}
+string[] Array = new string[7] { "hello", "2", "world", ":=)", "1234", "145", "-2j" };
+string[] NArray = new string[Array.Length]; // NArray it is "new array"
+
+newArray(Array, NArray);
+PrintArray(NArray);
